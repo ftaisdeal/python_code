b2a1c36ca89e5447735319fa933a5699ad41cc71
Firinn Taisdeal <firinn@taisdeal.com>
2022-04-27 14:09:15 -0400

    add code to temp.ps

--- temp.py
+++ temp.py
@@ -1,3 +1,14 @@
 # Defining a list
 import sys
 print(sys.version)
+
+# import numpy as np
+
+# a = np.array([1,2,3,4])
+# print(a[[False, True, False, False]])
+
+# x = list(reversed(range(1,11)))
+
+# for x in x :print(x)
+
+#print(2**10)
