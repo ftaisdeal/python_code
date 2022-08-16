40ef718c08995931b7dc0a033873c35dfdb9f82f
Firinn Taisdeal <44820542+ftaisdeal@users.noreply.github.com>
2022-08-16 17:25:37 -0400

    edit numpy1.py

--- numpy/numpy1.py
+++ numpy/numpy1.py
@@ -1,4 +1,11 @@
-import numpy
+import numpy as np
 
 array1 = [1, 2, 3, 4]
 array2 = [5, 6, 7, 8]
+
+np_array1 = np.array(array1)
+np_array2 = np.array(array2)
+
+bmi = np_array1 / np_array2 ** 2
+
+print(bmi)
